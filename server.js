1fb0fcd3524f7afbca091899466bb4c5931d4f5c
ryan10projects <rodricksryan10@gmail.com>
2022-07-23 12:37:25 +0530

    heroku deploy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use('/', routes)
+app.use('/api/blogs', routes)
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'))
